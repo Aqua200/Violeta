7ef752aaa87105dcf1ca24bb19a91712828a6cd5
Aqua200 <chinquepapa@gmail.com>
2024-10-24 19:35:02 -0700

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Daniel 🇦🇱;;\nFN:Daniel 🇦🇱\nORG:Daniel 🇦🇱\nTITLE:\nitem1.TEL;waid=51955918117:51955918117\nitem1.X-ABLabel:Daniel 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:𝑼𝒓𝒖𝒔❤️;;\nFN:𝑼𝒓𝒖𝒔❤️\nORG:𝑼𝒓𝒖𝒔❤️\nTITLE:\nitem1.TEL;waid=6282114987516:6282114987516\nitem1.X-ABLabel:𝑼𝒓𝒖𝒔❤️\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:𝑼𝒓𝒖𝒔❤️nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
