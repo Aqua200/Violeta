d664b3f874b912267ee988e3ab77278caf38369f
Aqua200 <chinquepapa@gmail.com>
2024-10-25 22:28:19 -0700

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,13 +1,16 @@
-import { spawn } from 'child_process'
-let handler = async (m, { conn, isROwner, text }) => {
-    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    if (conn.user.jid == conn.user.jid) {
-    await m.reply('🍭 Reiniciando Bot...')
-    process.send('reset')
-  } else throw 'eh'
-}
 
-handler.help = ['reiniciar']
+import { spawn } from 'child_process'
+
+var handler = async (m, { conn, isROwner, text }) => {
+
+if (!process.send) throw 'Dont: node luffy.js\nDo: node index.js'
+if (conn.user.jid == conn.user.jid) {
+await conn.reply(m.chat, '🌸 *R E I N I C I A N D O* 🌸', m, rcanal, )
+process.send('reset')
+} else throw 'eh'
+
+}
+handler.help = ['restart']
 handler.tags = ['owner']
 handler.command = ['restart','reiniciar'] 
 
