b02139fac2802934dd29b69b35634b080b244820
Aqua200 <chinquepapa@gmail.com>
2024-10-25 19:18:50 -0700

    Update rpg-minar.js

--- plugins/rpg-minar.js
+++ plugins/rpg-minar.js
@@ -1,19 +1,20 @@
-const cooldowns = {}
+
+let cooldowns = {}
 
 let handler = async (m, { conn }) => {
-
-  let amount = Math.floor(Math.random() * 20)
-  const tiempoEspera = 5 * 60 // 5 minutos
-  if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
-    const tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    m.reply(`🕜 Espera *${tiempoRestante}* para volver a Minar.`)
-    return
-  }
-
-  global.db.data.users[m.sender].limit += amount
-  await m.reply(`Genial! minaste *${amount} 🍬 Dulces*`)
-  cooldowns[m.sender] = Date.now()
+let dinero = Math.floor(Math.random() * 5000)
+let tiempo = 5 * 60
+if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
+let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
+conn.reply(m.chat, `🌸 Hola ${nombre}, Ya has minado recientemente, espera ⏱️ *${tiempo2}* para regresar a la Mina.`, m, rcanal)
+return
 }
+global.db.data.users[m.sender].exp += dinero
+let minar = `🚩 Genial! minaste *${dinero} 💫 XP.*`
+await m.react('⛏️')
+await conn.reply(m.chat, minar, m, rcanal)
+cooldowns[m.sender] = Date.now()}
+
 handler.help = ['minar']
 handler.tags = ['rpg']
 handler.command = ['minar', 'miming', 'mine'] 
@@ -21,7 +22,8 @@ handler.register = true
 export default handler
 
 function segundosAHMS(segundos) {
-  const minutos = Math.floor((segundos % 3600) / 60)
-  const segundosRestantes = segundos % 60
-  return `${minutos} minutos y ${segundosRestantes} segundos`
+let horas = Math.floor(segundos / 3600)
+let minutos = Math.floor((segundos % 3600) / 60)
+let segundosRestantes = segundos % 60
+return `${minutos} minutos y ${segundosRestantes} segundos`
 }
