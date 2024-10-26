3520afc2964c6f2522e6bc91abc6586006faceda
Aqua200 <chinquepapa@gmail.com>
2024-10-25 18:12:13 -0700

    Update rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,28 +1,42 @@
-import { canLevelUp, xpRange } from '../lib/levelling.js'
+
 import PhoneNumber from 'awesome-phonenumber'
+import fetch from 'node-fetch'
+var handler = async (m, { conn }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/32103b8335e64d253fa98.jpg')
+let { premium, level, estrellas, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[m.sender]
+let username = conn.getName(who)
+let noprem = `
+🚩 *PERFIL DE USUARIO*
+☁️ *Nombre:* ${username}
+💥 *Tag:* @${who.replace(/@.+/, '')}
+🌀 *Registrado:* ${registered ? '✅': '❌'}
 
-let handler = async (m, { conn, usedPrefix, command}) => {
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let user = global.db.data.users[who]
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-  let { exp, limit, name, registered, regTime, age, level } = global.db.data.users[who]
-  let { min, xp, max } = xpRange(user.level, global.multiplier)
-  let username = conn.getName(who)
-  let prem = global.prems.includes(who.split`@`[0])
-  let txt = `╭─⬣「 *User Perfil* 」⬣\n`
-     txt += `│  ≡◦ *🪴 Nombre ∙* ${name}\n`
-     txt += `│  ≡◦ *🐢 Edad ∙* ${age} años\n`
-     
-     txt += `│  ≡◦ *📞 Numero ∙* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}\n`
-     txt += `│  ≡◦ *🍬 Dulces ∙* ${limit}\n`
-     txt += `│  ≡◦ *💫 Experiencia ∙* Total ${exp} ( *${user.exp - min}/${xp}* )\n`
-     txt += `│  ≡◦ *👑 Premium ∙* ${prem ? 'Si' : 'No'}\n`
-     txt += `╰─⬣`
-await conn.sendFile(m.chat, pp, 'thumbnail.jpg', txt, m)
+👑 *RECURSOS*
+🌟 *Estrellas:* ${estrellas}
+💥 *Nivel:* ${level}
+💫 *Experiencia:* ${exp}
+✨️ *Rango:* ${role}
+
+💖 *Premium:* ${premium ? '✅': '❌'}
+`.trim()
+let prem = `╭──⪩ 𝐔𝐒𝐔𝐀𝐑𝐈𝐎 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ⪨
+│⧼👤⧽ *ᴜsᴜᴀʀɪᴏ:* 「${username}」
+│⧼💌⧽ *ʀᴇɢɪsᴛʀᴀᴅᴏ:* ${registered ? '✅': '❌'}
+│⧼🔱⧽ *ʀᴏʟ:* Vip 👑
+╰───⪨
+
+╭────⪩ 𝐑𝐄𝐂𝐔𝐑𝐒𝐎𝐒 ⪨
+│⧼🌟⧽ *ᴇsᴛʀᴇʟʟᴀs:* ${estrellas}
+│⧼🔰⧽ *ɴɪᴠᴇʟ:* ${level}
+│⧼💫⧽ *ᴇxᴘᴇʀɪᴇɴᴄɪᴀ:* ${exp}
+│⧼⚜️⧽ *ʀᴀɴɢᴏ:* ${role}
+╰───⪨ *𝓤𝓼𝓾𝓪𝓻𝓲𝓸 𝓓𝓮𝓼𝓽𝓪𝓬𝓪𝓭𝓸* ⪩`.trim()
+conn.sendFile(m.chat, pp, 'perfil.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, rcanal, { mentions: [who] })
 }
-handler.help = ['perfil', 'perfil @user']
-handler.tags = ['rg']
-handler.command = ['perfil', 'profile']
+handler.help = ['profile']
 handler.register = true
-
+//handler.group = true
+handler.tags = ['rg']
+handler.command = ['profile', 'perfil']
 export default handler
