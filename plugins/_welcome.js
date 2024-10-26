633e62b5cc7ab3449d58853f7229318988e21460
Aqua200 <chinquepapa@gmail.com>
2024-10-26 00:20:54 -0700

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,22 +3,20 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://qu.ax/jQRnm.png')
-  let img = await (await fetch(`${pp}`)).buffer()
+  let img = imagen1
   let chat = global.db.data.chats[m.chat]
 
-  if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `┌─★ *🩷𝑨𝒊𝒔𝒉𝒂 𝑮𝒓𝒆𝒚𝒓𝒂𝒕🩷* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
-    
-await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal)
+  if (chat.welcome && m.messageStubType == 27) {
+    let welcome = ` 🌸≺ 𝑨𝒊𝒔𝒉𝒂 𝑮𝒓𝒆𝒚𝒓𝒂𝒕 - MD \n「 Bιҽɳʋҽɳιԃσ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bιҽɳʋҽɳιԃσ/α 」\n「 ${groupMetadata.subject} 」\n\n  ιαɳαʅҽʝαɳԃɾσσƙ15x`
+await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
   }
-  
-  if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `┌─★ *🩷🩷𝑨𝒊𝒔𝒉𝒂 𝑮𝒓𝒆𝒚𝒓𝒂𝒕🩷🩷* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
+
+  if (chat.welcome && m.messageStubType == 28) {
+    let bye = ` 🌸≺ 𝑨𝒊𝒔𝒉𝒂 𝑮𝒓𝒆𝒚𝒓𝒂𝒕 \n「 Aԃισʂ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nυɳƈα ƚҽ ϙυιʂιɱσʂ αϙυι 」\n\n  ιαɳαʅҽʝαɳԃɾσσƙ15x`
+await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
   }
-  
-  if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `┌─★ *🩷𝑨𝒊𝒔𝒉𝒂 𝑮𝒓𝒆𝒚𝒓𝒂𝒕🩷* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal)
+
+  if (chat.welcome && m.messageStubType == 32) {
+    let kick = ` 🌸≺ 𝑨𝒊𝒔𝒉𝒂 𝑮𝒓𝒆𝒚𝒓𝒂𝒕 - MD \n「 Aԃισʂ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nυɳƈα ƚҽ ϙυιʂιɱσʂ αϙυι 」\n\n  ιαɳαʅҽʝαɳԃɾσσƙ15x`
+await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
 }}
